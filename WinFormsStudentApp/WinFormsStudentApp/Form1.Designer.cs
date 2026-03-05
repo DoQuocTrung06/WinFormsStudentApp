9b1b82c7d3c47829e66d3b8db07e413f0ebc8de0
Quoc Trung <doquoctrung2k@gmail.com>
2026-03-05 11:59:27 +0700

    Added Label and TextBox for Student Name

--- WinFormsStudentApp/WinFormsStudentApp/Form1.Designer.cs
+++ WinFormsStudentApp/WinFormsStudentApp/Form1.Designer.cs
@@ -28,18 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.txtStudentName = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(235, 129);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "StudentName:";
             // 
             // txtStudentName
             // 
@@ -48,13 +39,22 @@
             this.txtStudentName.Size = new System.Drawing.Size(156, 20);
             this.txtStudentName.TabIndex = 1;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "StudentName:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtStudentName);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStudentName);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -63,9 +63,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.MaskedTextBox txtStudentName;
+        private System.Windows.Forms.Label label1;
     }
 }
 
